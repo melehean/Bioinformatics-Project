add6ac54773b5e4a2b048ed66a2576a80cd6734d
Raaafal <rafal.wolniak.920@gmail.com>
2023-02-08 15:29:26 +0100

    fixed prepare_family_data function

--- data.py
+++ data.py
@@ -161,7 +161,7 @@ def prepare_family_data(drop_unknown_families=False):
     #print(family_data.columns)
     if drop_unknown_families:
         family_data = family_data[family_data['Family (Aggregated)'].str.contains('Unknown') == False]
-    names = family_data.iloc[:, 0]
+    #names = family_data.iloc[:, 0]
     #print(names)
     indices_to_names = {}
     for i, name in enumerate(family_data['Family (Aggregated)']):
@@ -177,4 +177,4 @@ def prepare_family_data(drop_unknown_families=False):
 
     family_data = family_data.reindex(sorted(family_data.columns), axis=1)
     family_data = family_data.apply(pd.to_numeric)
-    return family_data,names, indices_to_names
+    return family_data, indices_to_names
